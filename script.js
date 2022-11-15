1300931908947a1c11bb6c3aa2be483d00292b96
nininhosam <luizfelipeforc@gmail.com>
2022-11-15 19:31:24 -0300

    Added/Icon, Mouse Interactions, Sounds.

--- script.js
+++ script.js
@@ -7,60 +7,73 @@ let assortment = [
     "Work hard in silence; <br> Let success  make the noise",
     "Don't let yesterday take up too much of today",
     "You learn more from failure than from success. <br> Don't let it stop you, failure builds character",
-    "The most difficult thing is the decision to act, the rest is merely tenacity"
-
+    "The most difficult thing is the decision to act, the rest is merely tenacity",
 ]
-var getRandomNumber = () => Math.floor(Math.random() * assortment.length)
-let speech = document.querySelector("p#speech")
-let pet = document.querySelector("img#pet")
-let jar = document.querySelector("img#petfood")
-let bod = document.querySelector("body")
-let bowl = document.querySelector("img.bowl")
-
+var chirps = [
+    new Audio('assets/chirp1.mp3'),
+    new Audio('assets/chirp2.mp3')
+]
+let pour = new Audio('assets/pour.mp3');
+let shake = new Audio('assets/shake.mp3')
+let speech = document.querySelector("p#speech");
+let pet = document.querySelector("img#pet");
+let jar = document.querySelector("img#petfood");
+let bod = document.querySelector("body");
+let bowl = document.querySelector("img.bowl");
+var hungerTimer = null;
+var getRandomNumber = max => Math.floor(Math.random() * max);
 
 function changeSpeech() {
-    speech.innerHTML = assortment[getRandomNumber()]
+    chirps[getRandomNumber(chirps.length)].play();
+    speech.innerHTML = assortment[getRandomNumber(assortment.length)];
 }
-var speechTimer = setInterval(changeSpeech, 15000)
-
-
 function ask() {
     changeSpeech();
     clearInterval(speechTimer);
     speechTimer = setInterval(changeSpeech, 15000);
 }
-pet.addEventListener("click", ask)
-
-
 function grabJar() {
     if (bod.className == "") {
-        bod.classList.add('jar-active')
-        pet.style.removeProperty('cursor')
-        jar.style.removeProperty('cursor')
+        shake.play()
+        bod.classList.add('jar-active');
+        bod.style.cursor = `url('assets/jar_fixed.png'), auto`;
+        pet.style.removeProperty('cursor');
+        jar.style.removeProperty('cursor');
     }
 }
-jar.addEventListener("click", grabJar)
-
-
 function emptyBowl() {
-    bowl.setAttribute("src", "assets/bowl_empty.png")
-    bowl.setAttribute("id", "empty")
+    bowl.setAttribute("src", "assets/bowl_empty.png");
 }
 function clicked(e) {
     if (bod.className == "jar-active" && e.path[0].id != "petfood") {
         if (e.path[0].id == "bowl") {
-            bowl.setAttribute("src", "assets/bowl_full.png")
-            bowl.setAttribute("id", "full")
-            bod.className = ""
-            pet.style.cursor = "pointer"
-            jar.style.cursor = "grab"
-            setTimeout(emptyBowl, 30000)
+            pour.play();
+            bowl.setAttribute("src", "assets/bowl_full.png");
+            bod.className = "";
+            bod.style.removeProperty('cursor');
+            bowl.style.cursor = "pointer";
+            pet.style.cursor = "pointer";
+            jar.style.cursor = "grab";
+            clearInterval(hungerTimer);
+            hungerTimer = setInterval(emptyBowl, 30000);
         } else {
-            bod.className = ""
-            pet.style.cursor = "pointer"
-            jar.style.cursor = "grab"
+            bod.className = "";
+            bod.style.removeProperty('cursor');
+            bowl.style.cursor = "pointer";
+            pet.style.cursor = "pointer";
+            jar.style.cursor = "grab";
         }
     }
 }
-bowl.addEventListener("click", clicked)
-bod.addEventListener("click", clicked)
+function hovered() {
+    if (bod.className == "jar-active") {
+        bowl.style.cursor = `url('assets/jar_pour.png'), auto`;
+    }
+}
+
+var speechTimer = setInterval(changeSpeech, 15000);
+pet.addEventListener("click", ask);
+jar.addEventListener("click", grabJar);
+bowl.addEventListener("mouseenter", hovered);
+bowl.addEventListener("click", clicked);
+bod.addEventListener("click", clicked);
